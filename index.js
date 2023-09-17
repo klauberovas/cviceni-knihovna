021d3f5fce0d528c1584ea4c9457a45599a728ab
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-09-17 13:24:43 +0200

    objekty, media query

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ const book1 = {
     currency: 'czk',
   },
 };
+
+let library__info = document.getElementsByClassName('library__info');
+console.log(library__info.length);
+library__info[0].innerHTML +=
+  '<h2>' +
+  book1.name +
+  '</h2>' +
+  '<p>' +
+  book1.author +
+  '</p>' +
+  '<p>' +
+  book1.price.priceForSale +
+  ' ' +
+  book1.price.currency +
+  '</p>';
 // Vytvořte alespoň jednu další knihu se stejnými vlastnostmi ale jinými hodnotami.
 const book2 = {
   name: 'Vzpomínky na něj',
@@ -35,6 +50,21 @@ const book2 = {
   },
 };
 
+let library__info2 = document.getElementsByClassName('library__info');
+console.log(library__info.length);
+library__info[1].innerHTML +=
+  '<h2>' +
+  book2.name +
+  '</h2>' +
+  '<p>' +
+  book2.author +
+  '</p>' +
+  '<p>' +
+  book2.price.priceForSale +
+  ' ' +
+  book2.price.currency +
+  '</p>';
+
 const book3 = {
   name: 'To nejlepší v nás',
   author: 'Colleen Hover',
@@ -50,3 +80,18 @@ const book3 = {
     currency: 'czk',
   },
 };
+
+let library__info3 = document.getElementsByClassName('library__info');
+console.log(library__info.length);
+library__info[2].innerHTML +=
+  '<h2>' +
+  book3.name +
+  '</h2>' +
+  '<p>' +
+  book3.author +
+  '</p>' +
+  '<p>' +
+  book3.price.priceForSale +
+  ' ' +
+  book3.price.currency +
+  '</p>';
